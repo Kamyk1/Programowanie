058291df5ed301203313624351e1ba0c74d41613
Kamyk1 <kamil.krukowicz@op.pl>
2026-01-23 12:54:39 +0100

    SO2

---
+++ SO/Server/common.h
@@ -0,0 +1,18 @@
+//
+// Created by root on 1/14/26.
+//
+
+#include <stdint.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#ifndef SERVER_2026_01_14_COMMON_H
+#define SERVER_2026_01_14_COMMON_H
+
+#endif //SERVER_2026_01_14_COMMON_H
