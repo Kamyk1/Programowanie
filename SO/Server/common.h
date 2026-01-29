66d6b94f1a4055bcf73abfd12eaff12fec955ee0
Kamyk1 <kamil.krukowicz@op.pl>
2026-01-29 02:02:50 +0100

    sdsa

--- SO/Server/common.h
+++ SO/Server/common.h
@@ -1,64 +1,57 @@
 //
 // Created by root on 1/14/26.
 //
-#include <ctype.h>
 #include <fcntl.h>
 #include <pthread.h>
 #include <semaphore.h>
-#include <stdbool.h>
+#include <signal.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
-#include <time.h>
 #include <unistd.h>
 
-#define SHM_DATA "/shm_data"
 #define SHM_CONTROL "/shm_control"
+#define SHM_DATA "/shm_data"
 
 #ifndef SERVER_2026_01_14_COMMON_H
 #define SERVER_2026_01_14_COMMON_H
 
-enum operator_t
-{
-  MIN,
-  MAX
-};
+enum operator_t { MIN, MAX };
 
-struct control_t
-{
+struct control_t {
+  pid_t server_pid;
+
   enum operator_t op;
   size_t count;
+  int max_count;
+  int min_count;
   int32_t result;
-  size_t count_min;
-  size_t count_max;
 
   sem_t sem_server;
-  sem_t sem_request;
   sem_t sem_respond;
+  sem_t sem_request;
+
   pthread_mutex_t mutex;
 
   int server_status;
 };
 
-void error_handler(size_t error_code)
-{
-  switch (error_code)
-  {
+void error_handler(size_t error_code) {
+  switch (error_code) {
   case 1:
-    perror("shm_open");
+    perror("ERROR: FAILED TO SHM_OPEN");
     exit(1);
   case 2:
-    perror("failed to map");
+    perror("ERROR: FAILED TO TRUNCATE");
     exit(2);
   case 3:
-    perror("failed to truncate");
+    perror("ERROR: FAILED TO MMAP");
     exit(3);
   case 4:
-    printf("Server is busy");
+    printf("");
     exit(4);
   }
 }
-
 #endif // SERVER_2026_01_14_COMMON_H
